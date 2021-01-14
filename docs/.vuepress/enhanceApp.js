4764e78e4f9c9497385cd0203e82e79574780719
dinglianbo <845721213@qq.com>
2021-01-14 15:51:41 +0800

    vuepress

---
+++ docs/.vuepress/enhanceApp.js
@@ -0,0 +1,19 @@
+/*
+ * @Author: your name
+ * @Date: 2021-01-14 15:15:11
+ * @LastEditTime: 2021-01-14 15:15:22
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \rif-pc-antc:\Users\IT\Desktop\vue\vuepress-starter\docs\.vuepress\enhanceApp.js
+ */
+// 使用异步函数也是可以的
+export default ({
+  Vue, // VuePress 正在使用的 Vue 构造函数
+  options, // 附加到根实例的一些选项
+  router, // 当前应用的路由实例
+  siteData, // 站点元数据
+  isServer // 当前应用配置是处于 服务端渲染 或 客户端
+}) => {
+  // ...做一些其他的应用级别的优化
+}
+
---
+++ docs/README.md
@@ -0,0 +1,13 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-01-14 14:51:25
+ * @LastEditTime: 2021-01-14 15:05:55
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \rif-pc-antc:\Users\IT\Desktop\vue\vuepress-starter\docs\README.md
+-->
+# Hello VuePress
+
+```js
+let data = 1;
+```
